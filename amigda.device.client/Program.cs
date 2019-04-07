13c656ef084a0647125bbfe56ecdc49eb075d58d
Ifthekhar <hasan.ifthekhar@gmail.com>
2019-04-07 15:31:16 +1000

    Added Azure device client to send message to events hub

---
+++ amigda.device.client/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices.Client;
+
+namespace amigda.device.client
+{
+    class Program
+    {
+        private const string DeviceConnectionString = "HostName=ifhaiot.azure-devices.net;DeviceId=device-01;SharedAccessKey=u2OPfHnE4WdCaFDOr/KHVqDQmmsfuRccbp0GdiHkE+k=";
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                var deviceClient = DeviceClient.CreateFromConnectionString(DeviceConnectionString);
+                await deviceClient.OpenAsync();
+                var msseageBytes = Encoding.ASCII.GetBytes("hello from Amigda");
+                var message = new Message(msseageBytes);
+                await deviceClient.SendEventAsync(message);
+                Console.WriteLine("Device is connected");
+                Console.WriteLine("Hello World from Amigda");
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+            
+            Console.Read();
+        }
+    }
+}
